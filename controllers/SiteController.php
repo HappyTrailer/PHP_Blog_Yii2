30f4325ce44a35c856b95e17acf78bd6cc70df08
student <student@STEP.LAN>
2016-10-31 11:33:48 +0200

    commit

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -68,6 +68,11 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionIndex2()
+    {
+        return $this->render('index');
+    }
+
     /**
      * Login action.
      *
@@ -106,6 +111,19 @@ class SiteController extends Controller
      * @return string
      */
     public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionContact2()
     {
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
